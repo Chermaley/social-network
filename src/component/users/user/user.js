6b5d338e602bde98bae4d47497d1185f4eb35f54
Chermaley <slimix23@gmail.com>
2020-12-02 18:14:46 +0300

    add rest api (users page) / axios

--- src/component/users/user/user.js
+++ src/component/users/user/user.js
@@ -1,17 +1,38 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import classes from './user.module.scss';
 
-const User = ({fullName, status}) => {
+const User = ({fullName, onFollowClick, followStatus, location, photoUrl, status}) => {
+    const followUnfollow = followStatus ? 'unfollow' : 'follow';
     return (
         <div>
-            <p>{fullName}</p>
-            <p>{status}</p>
+            <span>
+                <div>
+                    <img src={photoUrl} alt='user photo' className={classes.photo}/>
+                </div>
+                <div>
+                    <button onClick={onFollowClick}>{followUnfollow}</button>
+                </div>
+            </span>
+            <span>
+                <span>
+                    <div>{fullName}</div><div>{status}</div>
+                </span>
+                <span>
+                    <div>{location}</div>
+                </span>
+            </span>
+
         </div>
     );
 };
 User.propTypes = {
     fullName: PropTypes.any,
-    status: PropTypes.any
+    status: PropTypes.any,
+    onFollowClick: PropTypes.func,
+    followStatus: PropTypes.bool,
+    photoUrl: PropTypes.string,
+    location: PropTypes.any
 
 };
 export default User;
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -1,27 +1,56 @@
-const FIND_USERS_BY_NAME = 'FIND_USERS_BY_NAME';
+const CHANGE_TERM = 'CHANGE_TERM';
+const TOGGLE_FOLLOW = 'TOGGLE_FOLLOW';
+const SET_USERS = 'SET_USERS';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
 
 const initialState = {
-    users: [
-        {id: 1, fullName: 'Dmitry', status:'help me', location: {city: 'Novosibirsk', country:'Russia'}},
-        {id: 2, fullName: 'Anton', status:'help me to', location: {city: 'Moscow', country:'Russia'}},
-        {id: 3, fullName: 'Elisey', status:'help me', location: {city: 'Yekaterinburg', country:'Russia'}},
-        {id: 4, fullName: 'Nick', status:'help me', location: {city: 'Minsk', country:'Belarus'}},
-        {id: 5, fullName: 'Artem', status:'help me', location: {city: 'Novosibirsk', country:'Russia'}}
-    ],
-    term: '1'
+    users: [],
+    pageSize: 100,
+    totalUsersCount: 0,
+    currentPage: 1,
+    term: ''
 };
 const usersReducer = (state = initialState, action) => {
-    let newUserList;
+
     switch (action.type) {
-        case FIND_USERS_BY_NAME:
-                newUserList = state.users.filter(state.users.indexOf(state.term) > 1);
-                return {
-                    ...state,
-                    users: newUserList
-                };
+        case SET_USERS:
+            return {
+                ...state,
+                users: [...action.users]
+            };
+        case TOGGLE_FOLLOW:
+             return {
+                 ...state,
+                 users: state.users.map((user) => {
+                     if (user.id === action.id) {
+                         return {...user, followed: !user.followed};
+                     }
+                     return user;
+                 })
+             };
+        case SET_TOTAL_USERS_COUNT:
+            return {
+                ...state,
+                totalUsersCount: action.usersCount
+            };
+        case SET_CURRENT_PAGE:
+            return {
+                ...state,
+                currentPage: action.pageNumber
+            };
+        case CHANGE_TERM:
+            return {
+                ...state,
+                term: action.term
+            };
         default:
             return state;
     }
 };
-export const findUser = () => ({type: FIND_USERS_BY_NAME});
+export const follow = (id) => ({type: TOGGLE_FOLLOW, id});
+export const changeTerm = (term) => ({type: CHANGE_TERM, term});
+export const setTotalUsersCount = (usersCount) => ({type: SET_TOTAL_USERS_COUNT, usersCount});
+export const setCurrentPage = (pageNumber) => ({type: SET_CURRENT_PAGE, pageNumber});
+export const setUsers = (users) => ({type: SET_USERS, users});
 export default usersReducer;
