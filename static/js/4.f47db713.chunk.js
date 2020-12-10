2efefc0a75d5ebf9e3f7bdf8782fc7065a1dbd9a
Chermaley <slimix23@gmail.com>
2020-12-10 20:09:24 +0300

    Updates

---
+++ static/js/4.f47db713.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[4],{290:function(p,s,t){"use strict";t.r(s);var e=t(0),n=(t(1),function(){return Object(e.jsx)("p",{children:"News"})});s.default=n}}]);
+//# sourceMappingURL=4.f47db713.chunk.js.map
