f1777f0ce138c16b7e1888ef15c4152b8adea553
Chermaley <slimix23@gmail.com>
2021-01-03 20:47:45 +0300

    add style for upload button

--- src/component/profile/profileInfo/profileLeftSide.tsx
+++ src/component/profile/profileInfo/profileLeftSide.tsx
@@ -2,7 +2,7 @@ import classes from "./profileInfo.module.scss";
 import userPhoto from "../../../assets/img/avatar.png";
 import React, {ChangeEvent} from "react";
 import {PhotosType} from "../../../types/types";
-
+import upload from "./upload.svg";
 
 type ProfileLeftSideType = {
     isOwner: boolean,
@@ -16,6 +16,10 @@ export const ProfileLeftSide: React.FC<ProfileLeftSideType> = ({isOwner, photos,
         <div className={classes.profilePhoto}>
             <img alt={'user photo'} src={photos.large || userPhoto}/>
         </div>
-        {isOwner && <input className={classes.upload} onChange={onPhotoSelected} type="file"/>}
+        {isOwner && <div className={classes.upload}>
+            <input  onChange={onPhotoSelected} type="file" id="actual-btn" hidden/>
+            <label htmlFor="actual-btn"><img src={upload} alt="upload"/></label>
+        </div>
+        }
     </div>;
 };
